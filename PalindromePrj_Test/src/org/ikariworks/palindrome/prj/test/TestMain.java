5f009ceeaba1212311b617a186ff580fd29b1377
shinjikun <leonardo.ilagan@gmail.com>
2012-09-25 01:46:52 +0800

    change test case scenarios

--- PalindromePrj_Test/src/org/ikariworks/palindrome/prj/test/TestMain.java
+++ PalindromePrj_Test/src/org/ikariworks/palindrome/prj/test/TestMain.java
@@ -1,3 +1,6 @@
+package org.ikariworks.palindrome.prj.test;
+
+
 import org.ikariworks.palindrome.prj.PalindromePrjActivity;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -20,14 +23,19 @@ public class TestMain extends
 	protected void tearDown() throws Exception {
 		solo.finishOpenedActivities();
 	}
-	public void testDisplayWhiteBox(){
+	public void testFirstConditionSuccess(){
 		solo.enterText(0, "abba");
 		solo.clickOnButton("Check!!!");
-		assertTrue(solo.searchText("Palindrome"));
+		assertTrue(solo.searchText("Yes it is"));
 	}
-	public void testNotTrue(){
+	public void testFirstConditionFail(){
 		solo.enterText(0, "hello");
 		solo.clickOnButton("Check!!!");
-		assertTrue(solo.searchText("Not Palindrome"));
+		assertTrue(solo.searchText("No it isn't"));
+	}
+	public void testSecondConditionFail(){
+		solo.enterText(0, "helldo");
+		solo.clickOnButton("Check!!!");
+		assertTrue(solo.searchText("No it isn't"));
 	}
 }
